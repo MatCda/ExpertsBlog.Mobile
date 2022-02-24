c40fe258c8db5d365e3213caf0d821120bbc6f5c
MatCda <mathieu.ramos@yahoo.fr>
2022-02-24 21:50:09 +0100

    camera permission

--- ExpertsBlog.Mobile/ExpertsBlog.Mobile/Pages/CameraPage.xaml.cs
+++ ExpertsBlog.Mobile/ExpertsBlog.Mobile/Pages/CameraPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExpertsBlog.Mobile.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,11 @@ namespace ExpertsBlog.Mobile.Pages
         public CameraPage()
         {
             InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ((CameraViewModel)BindingContext).Initialize();
         }
     }
 }
