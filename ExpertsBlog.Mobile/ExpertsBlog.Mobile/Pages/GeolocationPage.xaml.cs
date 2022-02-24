6aabdbeeed1e7509edbc53aae98bdbefb90e355b
MatCda <mathieu.ramos@yahoo.fr>
2022-02-24 10:53:51 +0100

    Implémentation geolocation

---
+++ ExpertsBlog.Mobile/ExpertsBlog.Mobile/Pages/GeolocationPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using ExpertsBlog.Mobile.ViewModels;
+using Xamarin.Forms;
+
+namespace ExpertsBlog.Mobile.Pages
+{
+    public partial class GeolocationPage : ContentPage
+    {
+        public GeolocationPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ((GeolocationViewModel)BindingContext).CheckAndRequestPermissions();
+        }
+    }
+}
